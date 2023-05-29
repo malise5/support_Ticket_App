671c3457770b3111293983fbbba174edcf4d8f20
Halkano Malise <kudezx@gmail.com>
2023-05-29 16:14:24 +0300

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./dbconfig/db");
 const port = process.env.PORT || 5000;
 const goalRoutes = require("./routes/goalRoutes");
 
+connectDB(); //running the database
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +19,6 @@ app.use(errorHandler); //middleware for handling errors and status codes
 //Port server
 app.listen(port, () => {
     console.log("====================================");
-    console.log(`Server Running on Port ${port}`);
+    console.log(`Server Running on Port ${port}`.cyan.underline);
     console.log("====================================");
 });
