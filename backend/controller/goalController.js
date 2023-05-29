bee4a44e612b6e5d0566d183e801c9764a535c3b
Halkano Malise <kudezx@gmail.com>
2023-05-29 15:30:03 +0300

    new pacckage asyncHandler added

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,47 +1,53 @@
+//======================OPTION 1 uSING eXPRESS aSYNC hANDLER================================
+const asyncHandler = require("express-async-handler");
 //@desc Get goals
 // @route GET/api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: "Get Goals",
     });
-};
+});
 
 //@desc Get goals
 // @route GET/api/goals/id
 // @access Private
-const getOneGoal = (req, res) => {
+const getOneGoal = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: `Get a specific goal ${req.params.id}`,
     });
-};
+});
 
 //@desc set goal
 // @route POST/api/goals
 // @access Private
-const setGaol = (req, res) => {
+const setGaol = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400);
+        throw new Error("fill the text field");
+    }
     res.status(201).json({
         message: "Set Goals",
     });
-};
+});
 
 //@desc Update goals
 // @route PUT/api/goals/:id
 // @access Private
-const updateGaol = (req, res) => {
+const updateGaol = asyncHandler(async (req, res) => {
     res.status(201).json({
         message: `Update a goal ${req.params.id}`,
     });
-};
+});
 
 //@desc Delete goals
 // @route DELETE/api/goals/:id
 // @access Private
-const deleteGaol = (req, res) => {
+const deleteGaol = asyncHandler(async (req, res) => {
     res.status(200).json({
         message: `Delete a goal ${req.params.id}`,
     });
-};
+});
 
 module.exports = {
     getGoals,
@@ -50,3 +56,91 @@ module.exports = {
     updateGaol,
     deleteGaol,
 };
+
+//======================OPTION 2 uSING TRY-CATCH Async================================
+// //@desc Get goals
+// // @route GET/api/goals
+// // @access Private
+// const getGoals = async (req, res) => {
+//     try {
+//       res.status(200).json({
+//         message: "Get Goals",
+//       });
+//     } catch (error) {
+//       res.status(500).json({
+//         error: "Server Error",
+//       });
+//     }
+//   };
+
+//   //@desc Get goals
+//   // @route GET/api/goals/id
+//   // @access Private
+//   const getOneGoal = async (req, res) => {
+//     try {
+//       res.status(200).json({
+//         message: `Get a specific goal ${req.params.id}`,
+//       });
+//     } catch (error) {
+//       res.status(500).json({
+//         error: "Server Error",
+//       });
+//     }
+//   };
+
+//   //@desc set goal
+//   // @route POST/api/goals
+//   // @access Private
+//   const setGaol = async (req, res) => {
+//     try {
+//       if (!req.body.text) {
+//         res.status(400);
+//         throw new Error("fill the text field");
+//       }
+//       res.status(201).json({
+//         message: "Set Goals",
+//       });
+//     } catch (error) {
+//       res.status(500).json({
+//         error: "Server Error",
+//       });
+//     }
+//   };
+
+//   //@desc Update goals
+//   // @route PUT/api/goals/:id
+//   // @access Private
+//   const updateGaol = async (req, res) => {
+//     try {
+//       res.status(201).json({
+//         message: `Update a goal ${req.params.id}`,
+//       });
+//     } catch (error) {
+//       res.status(500).json({
+//         error: "Server Error",
+//       });
+//     }
+//   };
+
+//   //@desc Delete goals
+//   // @route DELETE/api/goals/:id
+//   // @access Private
+//   const deleteGaol = async (req, res) => {
+//     try {
+//       res.status(200).json({
+//         message: `Delete a goal ${req.params.id}`,
+//       });
+//     } catch (error) {
+//       res.status(500).json({
+//         error: "Server Error",
+//       });
+//     }
+//   };
+
+//   module.exports = {
+//     getGoals,
+//     getOneGoal,
+//     setGaol,
+//     updateGaol,
+//     deleteGaol,
+//   };
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 const goalRoutes = require("./routes/goalRoutes");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.use("/api/goals", goalRoutes);
+
+app.use(errorHandler); //middleware for handling errors and status codes
 
 //Port server
 app.listen(port, () => {
