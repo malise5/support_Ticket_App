7128368ea02cbc20116ba2ef8b6277c63d682c15
Halkano Malise <kudezx@gmail.com>
2023-05-29 15:02:54 +0300

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("Hallo world");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+const goalRoutes = require("./routes/goalRoutes");
+
+const app = express();
+
+//Routes
+app.use("/api/goals", goalRoutes);
+
+//Port server
+app.listen(port, () => {
+    console.log("====================================");
+    console.log(`Server Running on Port ${port}`);
+    console.log("====================================");
+});
