ee0baea948be48a6a168f5bd4fe05fe315a95fcc
Halkano Malise <kudezx@gmail.com>
2023-05-30 23:40:19 +0300

    updated

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import GoalForm from "../components/GoalForm";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    const navigate = useNavigate();
+
+    const { user } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (!user) {
+            navigate("/login");
+        }
+    }, [user, navigate]);
+
+    return (
+        <>
+            <section className="heading">
+                <h1>welcome {user && user.name}</h1>
+                <p>Goals Dashboard</p>
+            </section>
+            <GoalForm />
+        </>
+    );
 };
 
 export default Dashboard;
