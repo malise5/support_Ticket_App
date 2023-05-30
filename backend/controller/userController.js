de98a1aed39aebf1223e9f3a2e9f26605739719e
Halkano Malise <kudezx@gmail.com>
2023-05-30 23:39:22 +0300

    fixed bug

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -71,13 +71,9 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET/api/users/me
 // @access private
 const getMe = asyncHandler(async (req, res) => {
-    const { _id, name, email } = await User.findById(req.user.id);
+    // const { _id, name, email } = await User.findById(req.user.id);
 
-    res.status(200).json({
-        id: _id,
-        name: name,
-        email: email,
-    });
+    res.status(200).json(req.user);
 });
 
 //Genertae JWT
